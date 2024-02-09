10f61854f60b0c732fa079f3ba910f0f8b6703ab
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-02-10 05:09:23 +0800

    Add piechart Component, missing styling and some logic

--- src/components/reportComponents/ByDemographicsComponent.tsx
+++ src/components/reportComponents/ByDemographicsComponent.tsx
@@ -1,12 +1,67 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
+import demographics from '../../utilities/AllDemographics';
+import ChartComponent from './chartsComponents/ChartComponent';
+
+interface DemographicType {
+    value: string;
+    label: string;
+}
+
+const AllDemographics: DemographicType[] = demographics;
 
 const ByDemographicsComponent: React.FC = () => {
 
+    const [demographic, setDemographic] = useState<DemographicType[] | null>(null);
+
+    const handleDownloadPress = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        console.log(demographic);
+    }
+
+    const handleDemographicChange = (selectedOption: any) => {
+        setDemographic(selectedOption);
+        if (selectedOption.length === 0) {
+            setDemographic(null);
+        }
+    }
+
     return (
-        <div>
-            <h2>By Demographics</h2>
+        <div style={{ padding: "3%" }}>
+            <form onSubmit={handleDownloadPress} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+                    <label style={{ flex: '0 0 30%' }}>
+                        <Select
+                            value={demographic}
+                            placeholder="Select demographic(s)"
+                            onChange={handleDemographicChange}
+                            options={AllDemographics}
+                            isMulti={true} // allow multiple selections
+                            className="basic-multi-select"
+                            classNamePrefix="select"
+                            styles={{ control: (base) => ({ ...base, height: '50px' }) }}
+                        />
+                    </label>
+                    <button className="form-control" type="submit" style={{ flex: '0 0 20%', height: '50px' }}>Download</button>
+                </div>
+            </form>
+            {
+                demographic && (
+                    <div style={{ padding: '2%' }}>
+                        {
+                            demographic.map((demo, index) => (
+                                <div key={index} style={{ border: '1px solid black', alignContent:'center', alignItems:'center'}}>
+                                    <h2>Demographic: {demo.label}</h2>
+                                    <ChartComponent />
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
+
     );
 };
 
---
+++ src/utilities/AllDemographics.tsx
@@ -0,0 +1,7 @@
+const AllDemographics = [
+  { value: 'gender', label: 'Gender' },
+  { value: 'female', label: 'Age' },
+  { value: 'non_binary', label: 'Employment Status' },
+];
+
+export default AllDemographics;
